c0563016c9eedd37c2e3c447b5fc9e893da91c23
anandsolugenix <anand.baghel@solugenix.com>
2024-08-23 10:49:41 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ApiServiceService } from './core/api-service.service';
+import { SpinnerComponent } from "./shared/spinner/spinner.component";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SpinnerComponent],
+  
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'idea-portal';
+
+  constructor(private _apiService: ApiServiceService) {
+    _apiService.get('/todos/1').subscribe(res => {
+      console.log(res);
+      
+    })
+  }
 }
